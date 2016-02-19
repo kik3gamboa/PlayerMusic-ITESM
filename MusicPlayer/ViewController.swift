dd916be331e2a7a23923b6ca50cfa150050bf266
Kike Gamboa <kikeremiau@gmail.com>
2016-02-19 12:48:32 -0600

    Initial Commit

---
+++ MusicPlayer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MusicPlayer
+//
+//  Created by Telecomunicaciones Abiertas de México on 2/19/16.
+//  Copyright © 2016 Telecomunicaciones Abiertas de México. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
